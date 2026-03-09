74b4ba65b9fe61d1d4a99866c0f6b0b14188927b
visartchu <145192388+visartchu@users.noreply.github.com>
2026-03-09 16:07:09 +0100

    Update data.js

--- data.js
+++ data.js
@@ -36,13 +36,13 @@ const items = [
   { id: 11, name: "PEBRE NEGRE MOLT", price: 1.30, category: "comun", consumers: "all" },
   { id: 12, name: "HIGIENIC DOBLE ROLL", price: 4.50, category: "comun", consumers: "all" },
   { id: 13, name: "CALÇOTS", price: 40.00, category: "comun", exclude: ["Berni", "Vera"] },
-  { id: 14, name: "OLIVAS", price: 4.89, category: "comun", consumers: "all" },
+  { id: 14, name: "OLIVAS", price: 4.89, category: "comun", exclude: ["Isa"] },
   { id: 15, name: "PATATAS", price: 5.98, category: "comun", consumers: "all" },
   { id: 16, name: "PATATAS CAMPESINAS", price: 2.05, category: "comun", consumers: "all" },
   { id: 17, name: "RUFFLES JAMÓN", price: 2.99, category: "comun", consumers: "all" },
   { id: 18, name: "PAPEL DE PLATA", price: 3.34, category: "comun", consumers: "all" },
   { id: 19, name: "BOLSAS DE BASURA", price: 2.10, category: "comun", exclude: ["Ruben", "Eric"] },
   { id: 20, name: "ESTROPAJO", price: 2.00, category: "comun", consumers: "all" },
-  { id: 21, name: "HIELOS", price: 5.50, category: "comun", consumers: "all" },
+  { id: 21, name: "HIELOS", price: 5.50, category: "comun", exclude: ["Isa"] },
   { id: 22, name: "PASTILLAS CARBON", price: 1.05, category: "comun", consumers: "all" }
 ];
