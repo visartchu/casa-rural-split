aeff33caff42924640662796bf73bfe00cf4d29a
visartchu <145192388+visartchu@users.noreply.github.com>
2026-03-09 15:45:45 +0100

    Update data.js

--- data.js
+++ data.js
@@ -41,7 +41,7 @@ const items = [
   { id: 16, name: "PATATAS CAMPESINAS", price: 2.05, category: "comun", consumers: "all" },
   { id: 17, name: "RUFFLES JAMÓN", price: 2.99, category: "comun", consumers: "all" },
   { id: 18, name: "PAPEL DE PLATA", price: 3.34, category: "comun", consumers: "all" },
-  { id: 19, name: "AGUA", price: 3.98, category: "comun", consumers: "all" },
+  { id: 19, name: "AGUA", price: 3.98, category: "comun", exclude: ["Ruben", "Eric"] },
   { id: 20, name: "ESTROPAJO", price: 2.00, category: "comun", consumers: "all" },
   { id: 21, name: "HIELOS", price: 5.50, category: "comun", consumers: "all" }
 ];
