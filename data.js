e36ba608bea102644006b193a08cbbdbdde0725c
visartchu <145192388+visartchu@users.noreply.github.com>
2026-03-14 12:23:03 +0100

    Update data.js

--- data.js
+++ data.js
@@ -19,19 +19,20 @@ const paid = {
   Vera: 23.23,
   Jordi: 2,
   Alvaro: 180.8,
-  Berni: 5.50
+  Berni: 5.50,
+  RUben: 5.00
 };
 const items = [
   { id: 1, name: "CARBO VEGETAL", price: 4.25, category: "comun", consumers: "all" },
   //{ id: 2, name: "BOSSA PLASTIC", price: 1.20, category: "comun", consumers: "all" },
-  { id: 3, name: "OLI VERGE", price: 4.45, category: "comun", exclude: ["Berni", "Vera"] },
+  { id: 3, name: "OLI VERGE", price: 4.45, category: "comun", exclude: ["Berni", "Vera","Ruben","Eric"] },
   { id: 4, name: "ROTLLIE CUINA GEGANT", price: 2.90, category: "comun", consumers: "all" },
   { id: 5, name: "SAL FINA", price: 0.40, category: "comun", consumers: "all" },
   { id: 6, name: "ALL SEC 250 G", price: 1.85, category: "comun", consumers: "all" },
-  { id: 7, name: "ALLOTI TARRINA", price: 1.10, category: "comun", exclude: ["Berni", "Vera"] },
+  { id: 7, name: "ALLOTI TARRINA", price: 1.10, category: "comun", exclude: ["Berni", "Vera","Ruben","Eric"] },
   { id: 8, name: "GOT REUTILITZABLE", price: 1.65, category: "comun", exclude: ["Ruben","Eric"] },
   { id: 9, name: "SALSA CALÇOTS (4)", price: 11.20, category: "comun", exclude: ["Berni", "Vera"] },
-  { id: 10, name: "OLIVA AMB OS PET", price: 1.40, category: "comun", consumers: "all" },
+  { id: 10, name: "OLIVA AMB OS PET", price: 1.40, category: "comun", exclude: ["Ruben","Eric"] },
   { id: 11, name: "PEBRE NEGRE MOLT", price: 1.30, category: "comun", consumers: "all" },
   { id: 12, name: "HIGIENIC DOBLE ROLL", price: 4.50, category: "comun", consumers: "all" },
   { id: 13, name: "CALÇOTS", price: 40.00, category: "comun", exclude: ["Berni", "Vera"] },
@@ -42,6 +43,7 @@ const items = [
   { id: 18, name: "PAPEL DE PLATA", price: 3.34, category: "comun", consumers: "all" },
   { id: 19, name: "BOLSAS DE BASURA", price: 2.10, category: "comun", exclude: ["Ruben", "Eric"] },
   { id: 20, name: "ESTROPAJO", price: 2.00, category: "comun", consumers: "all" },
+  { id: 20, name: "COSAS RUBEN (Cafe, Jabón,Agua,Ketchup,Tomate", price: 5.00, category: "comun", consumers: "all" },
   { id: 21, name: "HIELOS", price: 5.50, category: "comun", exclude: ["Isa","Ruben","Eric"] },
   { id: 22, name: "PASTILLAS CARBON", price: 1.05, category: "comun", consumers: "all" },
   { id: 23, name: "RON VIERNES", price: 15.45, category: "alcohol_viernes", exclude: ["Alba", "Eric", "Nora", "Paula", "Ruben", "Isa", "Jordi", "Maria", "Sergi","Berni"] },
